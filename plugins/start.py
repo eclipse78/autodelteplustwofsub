dbd166b574f758ef1ecc69dd5af2ff9b03506e4d
eclipse78 <132812659+eclipse78@users.noreply.github.com>
2024-04-29 09:07:02 +0000

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
+from config import ADMINS, FORCE_MSG, START_MSG, START_PIC, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
@@ -99,27 +99,27 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
         return
-    else:
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("about", callback_data = "about"),
-                    InlineKeyboardButton("close", callback_data= "close")
-                ]
-            ]
-        )
-        await message.reply_text(
-            text = START_MSG.format(
+        
+        @Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+    await madflixbotz.add_user(client, message)                
+    button = InlineKeyboardMarkup([[
+      InlineKeyboardButton('Update Channel', url='https://t.me/AnimeXWrld'),
+      InlineKeyboardButton('Support Group', url='https://t.me/AnimeXWrld_Chat')
+    ],[
+      InlineKeyboardButton('about', callback_data='about'),
+      InlineKeyboardButton('close', callback_data='close')
+    ]])
+    if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
                 mention = message.from_user.mention,
                 id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
+            ), reply_markup=button)       
+    else:
+        await message.reply_text(text = START_MSG.format(user.mention), reply_markup=button, disable_web_page_preview=True)
         return
 
     
