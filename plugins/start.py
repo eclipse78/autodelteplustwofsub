5ab37e968ef2c2bd27a6e310d949cdd905d744f7
eclipse78 <132812659+eclipse78@users.noreply.github.com>
2024-04-29 09:34:23 +0000

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -107,14 +107,18 @@ async def start_command(client: Client, message: Message):
       InlineKeyboardButton('about', callback_data='about'),
       InlineKeyboardButton('close', callback_data='close')
     ]])
-         await message.reply_photo(photo=START_PIC, text = START_MSG.format(
+         await message.reply_text(
+            text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
                 mention = message.from_user.mention,
                 id = message.from_user.id
-            ), reply_markup = reply_markup,disable_web_page_preview = True,
-            quote = True)
+            ),
+            reply_markup = reply_markup,
+            disable_web_page_preview = True,
+            quote = True
+        )
         return
 
     
